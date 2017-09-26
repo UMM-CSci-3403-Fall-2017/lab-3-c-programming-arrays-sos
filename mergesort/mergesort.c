d8a228136c5675a74d4e0f0984e23ede742a53e6
holma198 <holma198@morris.umn.edu>
2017-09-26 11:43:22 -0500

    Add mergesort.c, has some errors, but entirety of code is written to run mergesort. Because we follow a java implementation, we add memory allocations.

---
+++ mergesort/mergesort.c
@@ -0,0 +1,56 @@
+#include "mergesort.h"
+#include <stdlib.h>
+
+bool needsSorting(int rangeSize) {
+        return rangeSize >= 2;
+}
+
+void mergeRanges(int* values, int start, int mid, int end){
+	int rangeSize = end - start;
+	int* destination = (int*) calloc(rangeSize+1, sizeof(int));
+	int firstIndex = start;
+	int secondIndex = mid;
+	int copyIndex = 0;
+	while (firstIndex < mid && secondIndex < end){
+		if(values[firstIndex] < values[secondIndex]) {
+			destination[copyIndex] = values[firstIndex];
+			++firstIndex;	
+		}
+		else{
+			destination[copyIndex] = values[secondIndex];
+			++secondIndex;
+		}
+		++copyIndex;
+	}
+	while (firstIndex < mid) {
+		destination[copyIndex] = values[firstIndex];
+		++copyIndex;
+		++firstIndex;
+	}
+	while (secondIndex < end) {
+		destination[copyIndex] = values[secondIndex];
+		++copyIndex;
+		++secondIndex;
+	}
+	for (int i = 0; i < rangeSize; ++i) {
+		values[i + startIndex] = destination[i];
+	}
+}
+
+void mergesortRange(int* values, int start, int end) {
+        int rangeSize = end - start;
+        int* outerArr;
+        if(needsSorting(rangeSize)) {
+                int mid = (start + end)/2;
+                mergesortRange(values, start, mid);
+                mergesortRange(values, mid, end);
+
+        }
+}
+
+
+void mergesort(int size, int* values){
+        mergesortRange(values, 0, size);
+}
+
+
