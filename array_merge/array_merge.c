f94e1a8a33d8c57b8dfbd17714c75a8b6c27f5bc
holma198 <holma198@morris.umn.edu>
2017-10-03 11:52:22 -0500

    Fix issue that prevented a single item array with an array of size one from passing the test.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,14 +1,18 @@
 #include "array_merge.h"
 #include <stdlib.h>
-#include "mergesort.h"
+#include "../mergesort/mergesort.h"
+#include <stdio.h>
 
 int* array_merge(int num_arrays, int* sizes, int** values){
-	
 	int allElementsLength = 0;	
 	//the following for loop sorts all the elements in each subarray
 	//in the matrix.
+	if(num_arrays < 2 && sizes[0] == 1){
+		allElementsLength = 1;
+	} else{
 	for(int i = 0; i < num_arrays; i++){
 		allElementsLength += sizes[i];
+	}
 	}
 	//do gross thing that checks the number of unique numbers to get
 	//size for the calloc, then calloc two arrays, one for the used unique
@@ -16,13 +20,12 @@ int* array_merge(int num_arrays, int* sizes, int** values){
 	//
 	//EASIER SOLUTION, add all elements into one big array of size
 	//num_arrays*(sum of sizes), then do aformentioned steps.
-	int* allElements = calloc(allElementsLength, sizeof(int));
+	int* allElements = (int*)calloc(allElementsLength, sizeof(int));
 	int allElementCounter = 0;
 	for(int i = 0; i < num_arrays; i++){
 		allElementCounter++;
 		for(int p = 0; p < sizes[i]; p++){
 			allElements[allElementCounter] = values[i][p];
-			printf(values[i][p]);
 			allElementCounter++;
 		}
 	}		
@@ -36,24 +39,37 @@ int* array_merge(int num_arrays, int* sizes, int** values){
 			uniqueNum++;
 		}
 	}
+	
+	int* returnArr;
 	int returnArrCounter = 0;
-	int* returnArr = calloc(uniqueNum, sizeof(int));
-	for (int i = 0;i<allElementsLength; i++) {
-                if(i == 0){
-                        returnArr[returnArrCounter] = uniqueNum;
-			returnArrCounter++;
-                }
-		else if(i == 1){
-			returnArr[returnArrCounter] = allElements[0];
-			returnArrCounter++;
-		}
-		else if(allElements[i-1]!=allElements[i]){
-                        returnArr[returnArrCounter] = allElements[i];
-			returnArrCounter++;
-                }
-
+	if(num_arrays == 1 && sizes[0] == 1){
+		returnArr =(int*) calloc(2, sizeof(int));
+		returnArr[0] = 1;
+		returnArr[1] = values[0][0];
         }
+        else if(num_arrays == 0){
+		returnArr =(int*) calloc(1, sizeof(int));
+		returnArr[0] = 0;
+	}
+	else{
+		returnArr =(int*) calloc(uniqueNum, sizeof(int));
+		for (int i = 0;i<allElementsLength; i++) {
+                	if(i == 0){
+                        	returnArr[returnArrCounter] = uniqueNum;
+				returnArrCounter++;
+                	}
+			else if(i == 1){
+				returnArr[returnArrCounter] = allElements[0];
+				returnArrCounter++;
+			}
+			else if(allElements[i-1]!=allElements[i]){
+                        	returnArr[returnArrCounter] = allElements[i];
+				returnArrCounter++;
+                	}
 
+        	}
+	}
+	return returnArr;
 }
 
 
