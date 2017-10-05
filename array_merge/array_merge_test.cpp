31148a3245352c6500254d02f6666a5bfb9ce773
holma198 <holma198@morris.umn.edu>
2017-10-05 11:14:27 -0500

    Add all necessary free statements for the tests to ensure no memory leaks exist

--- array_merge/array_merge_test.cpp
+++ array_merge/array_merge_test.cpp
@@ -93,8 +93,10 @@ TEST(ArrayMerge, Handle_different_sizes) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
-  //free(result);
-  //free(a);
+  free(result);
+  for(int i = 0; i < 11; i++){
+	free(a[i]);
+  }
 }
 
 TEST(ArrayMerge, Handle_different_sizes_reversed) {
@@ -115,8 +117,10 @@ TEST(ArrayMerge, Handle_different_sizes_reversed) {
 
   result = array_merge(num_arrays, sizes, a);
   arrays_match(11, result, expected);
-  //free(result);
-  //free(a);
+  free(result);
+  for(int i = 0; i < 11; i++){
+	free(a[i]);
+  }
 
 }
 
