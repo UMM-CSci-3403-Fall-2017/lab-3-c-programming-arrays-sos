4a8505e73a852a9944d5d44c724a36f77a7c0b7a
holma198 <holma198@morris.umn.edu>
2017-10-05 11:12:03 -0500

    Fix error of memory allocation, in which only the number of unique elements was returned for the calloc when it needed to be unique elements + 1, to account for the size being the first element.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -5,23 +5,16 @@
 
 int* array_merge(int num_arrays, int* sizes, int** values){
 	int allElementsLength = 0;	
-	//the following for loop sorts all the elements in each subarray
-	//in the matrix.
-	if((num_arrays < 2 && sizes[0] == 1) || num_arrays == 0){
-		allElementsLength = 1;
-	} else{
+	//the following for loop returns the number of all the elements
+	//in every array.
 	for(int i = 0; i < num_arrays; i++){
 		allElementsLength += sizes[i];
 	}
-	}
-	//do gross thing that checks the number of unique numbers to get
-	//size for the calloc, then calloc two arrays, one for the used unique
-	//numbers, and an array with the total number of elements.  
-	//
-	//EASIER SOLUTION, add all elements into one big array of size
-	//num_arrays*(sum of sizes), then do aformentioned steps.
+	//A new array is created to store every element in each subarray.
 	int* allElements = (int*)calloc(allElementsLength, sizeof(int));
 	int allElementCounter = 0;
+	//This for loop adds each element from each subarray into the newly
+	//defined array
 	for(int i = 0; i < num_arrays; i++){
 		for(int p = 0; p < sizes[i]; p++){
 			allElements[allElementCounter] = values[i][p];
@@ -29,7 +22,11 @@ int* array_merge(int num_arrays, int* sizes, int** values){
 		}
 	}		
 	int uniqueNum = 0;
+	//The array of every element is sorted
 	mergesort(allElementsLength, allElements);
+	//The sorted array of every element is checked for unique elements
+	//which when sorted will simply be the next new element.  This number
+	//of unique elements is stored in variable uniqueNum.
 	for (int i = 0;i<allElementsLength; i++) {
 		if(i == 0){
 			uniqueNum++;
@@ -38,20 +35,29 @@ int* array_merge(int num_arrays, int* sizes, int** values){
 			uniqueNum++;
 		}
 	}
-	
+	//The following conditionals check for scenarios that need special
+	//return values.
 	int* returnArr;
 	int returnArrCounter = 0;
+	//This condition is met with a single array of one element
+	//only the size and element are returned.
 	if(num_arrays == 1 && sizes[0] == 1){
 		returnArr =(int*) calloc(2, sizeof(int));
 		returnArr[0] = 1;
 		returnArr[1] = values[0][0];
         }
+	//This condition is met when an empty array is passed in, only
+	//the size is returned.
         else if(num_arrays == 0){
 		returnArr =(int*) calloc(1, sizeof(int));
 		returnArr[0] = 0;
 	}
+	//This condition is met when the previous two conditions are not,
+	//a return array is stored in memory, and each unique element is 
+	//added to the return array from the already sorted allElements list
+	//thus returning a sorted array of unique elements.
 	else{
-		returnArr =(int*) calloc(uniqueNum, sizeof(int));
+		returnArr =(int*) calloc(uniqueNum + 1, sizeof(int));
 		for (int i = 0;i<allElementsLength; i++) {
                 	if(i == 0){
                         	returnArr[returnArrCounter] = uniqueNum;
