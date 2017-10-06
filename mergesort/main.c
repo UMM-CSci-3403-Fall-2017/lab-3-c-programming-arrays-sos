d754c432779ed2e1f4d17a9a9ba5dfee1abbc7b4
holma198 <holma198@morris.umn.edu>
2017-10-06 15:41:51 -0500

    Main method which simply prints a series of lines that make up the resulting 'merged' array

--- mergesort/main.c
+++ mergesort/main.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// This is a main method which simply makes a sample array of numbers,
+// sorts it, and then makes an array of characters to be printed.
 int main(int argc, char* argv[]) {
 	int line[5] = {8,4,9,4,3};
-	char* returnStr = "[";
-
+	char* returnStr = calloc(100, sizeof(char));
+	//returnStr[0] = '[';
+	printf("%s\n",returnStr);
 	mergesort(5, line);
 
 	for(int i = 0; i < 5; i++){
-		returnStr = returnStr + line[i] + ',' + ' ';
-		if(i == 4){
-                        returnStr = returnStr + line[i] + ']';
-                }
+		printf("%d\n",line[i]);
         }
-
-	printf(returnStr,"\n");
+	returnStr = returnStr + '\0';
+	printf("%s\n", returnStr);
+	printf("%s\n","asdf");
 	
 	
 
